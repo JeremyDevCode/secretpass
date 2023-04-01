9bf9c9d57f1273c0c893f810a725d0439bae16f7
JeremyDevCode <jeremydevcode@gmail.com>
2023-04-01 00:26:36 -0500

    Fix card mobile

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Card({ children }) {
   return (
-    <article className='flex flex-col m-4 p-9 rounded-2xl w-[460px] gap-8 bg-white'>
-    {children}
+    <article className="flex flex-col m-4 p-9 rounded-2xl w-11/12 sm:w-[460px] gap-8 bg-white">
+      {children}
     </article>
-  )
+  );
 }
 
-export { Card }
+export { Card };
